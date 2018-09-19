38a41f4994161c46aba282b84be49f1f986ced92
Travis Desir <travisgdesir31@gmail.com>
2018-09-19 01:38:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   words = string.split(" ")
   array = []
-  words.collect { |turn| array << turn.reverse}
+  words.collect { |turn| array.push(turn.reverse)}
   array.join(" ")
 end
